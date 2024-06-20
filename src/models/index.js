c682f3a9ee577c51f1249693d955102529997de8
RasyaPrtma <rasyaputraq17@gmail.com>
2024-06-20 08:35:37 +0700

    Add

--- src/models/index.js
+++ src/models/index.js
@@ -12,7 +12,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'admin',
+    password: '',
     database: 'apilsp',
     waitForConnections: true,
     connectionLimit: 10,
